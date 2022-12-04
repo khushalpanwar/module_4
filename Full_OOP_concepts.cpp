5e4e7cb9d4f3f15c0c078e808eaf6cec53e3d2fc
khushalpanwar <118703716+khushalpanwar@users.noreply.github.com>
2022-12-04 10:41:42 +0530

    Add files via upload

---
+++ Full_OOP_concepts.cpp
@@ -0,0 +1,90 @@
+/* what is OOP? List OOP concepts.
+
+==> Object oriented programming is a programming paradigm 
+    based on the concept of "objects", which can contain
+    data and code:
+    data in the form of fields, and code,in the form of 
+    procedures. A common features of objects in that 
+    procedures are attached to them and can access and
+    modify the object's data fields.
+    
+    1. Inheritance==> In object-oriented programming, inheritance
+        is a mechanism where programmers can derive a class from
+        another class. This concept of OOP can be useful in giving
+        custom logic to existing frameworks and in declaring different
+        exceptions. Inheritance also allows programmers to reuse previously
+        writtenf codes. This removes the burden of writing the same codes again,
+        as programmers can make a derived class inherit the property of its
+        parent class. For example, one can create two classes and name them
+        hatchback and sedan inherited from the parent class car.
+        
+        There are following five different variations in inheritance in OOP languages:
+        
+        * Single inheritance: It is the simplest form of inheritance where a class 
+          inherits only one parent class. Single inheritance enables code reusanility 
+          and adds new features to the existing class.
+        
+        * Multiple inheritance: When a class inherits more than one parent class,it
+          becomes multiple inheritance. As the child class inherits properties from
+          different parent classes, it has access to all of its objects. It is the 
+          diffrent from a single inheritance property, as it allows an object or class
+          to inherit from more than one object or class.
+        
+        * Multilevel inheritance: When one class inherits properties from a derived
+          class, it is multilevel inheritance. For example, class A extends class B
+          and class B extend class C.
+          
+        * Hierarchical inheritance: In this variation of inheritance, the different 
+		  child classes inherit a single parent class. For example, a parent class C 
+		  can have three subclasses,D,E and F.
+		  
+		* Hybrid inheritance: If there is a combination of more than one type of 
+		  inheritance, it is a hybrid inheritance. It can be a combination of simple,
+		  multiple and hierarchical inheritances.
+		  
+		  
+	2. Polymorphism ==>
+	   Polymorphism is the core concept of the object-oriented programming language
+	   that allows programmers to build logical codes. In this concept of OOP,
+	   programmers can access objects of different types through the same interface 
+	   where each type provides its own implementation of the interface.
+	   
+	   The following two types of Polymorphism are useful in OOP.
+	   
+		* Compile-time Polymorphism : Also called static binding Polymorphism, it means
+	     binding occurs at compile time. Method overloading is an example of compile-time
+	     Polymorphism. It allows programmers to use objects of the same name while their 
+	     parameters can be different.
+	     
+		* Runtime Polymorphism: Runtime Polymorphism involves dynamic binding. Method 
+		overriding is an example of runtime Polymorphism. In this process, an object binds
+		with the functionality at the run time.
+		
+	3. Abstraction ==>
+	   This is the core concept of OOP that allows programmers to be abstract or pick out
+	   common features of the objects and procedures. In other words, abstraction means hiding
+	   intenal details and showing functionality. The primary aim of programmers behind
+	   using abstraction in to handle complexity by irrelevant details. It is an extension of 
+	   encapsulation.
+	   
+	   For example, a customer may only use a few selections of tools petrol,accelerator,clutch,
+	   brake,wheel,and odometer of a motorcycle. Most of the engineering work remains inconsequential
+	   to the customer till it continues to function properly. To make a motorcycle run, a lot of 
+	   pieces and parts work together. Letting the details and information get exposed to the 
+	   customers can be a dangerous distraction. This is the concept of abstraction that is useful in 
+	   many fields of engineering.
+	   
+	4. Encapsulation ==>
+		It is the basic concept of OOP that helps programmers to bind the data and functions together 
+		that manipulate the data. Encapsulation of data leads to the OOP concept of data hiding and 
+		keeps the safe from outside attention. One of the common examples of encapsulation is calculator,
+		as anybody using a calculator understands its functions, but may not require an understanding of 
+		how it works inside. Encapsulation can help in hiding irrelevant details from the outside world 
+		and highlight the necessary characteristics of a class to the users.
+*/
+
+        
+        
+        
+        
+        
---
+++ difference_OOP_POP.cpp
@@ -0,0 +1,44 @@
+What is the difference between OOP and POP ?
+
+OOP
+
+OOP,refers to Object Oriented Programming and its deals with objects
+and their properties. Major concepts of OOPs are --
+		- Class/objects
+		- Abstraction
+		- Encapsulation
+		- Polymorphism
+		- Inheritance
+		
+POP
+
+POP,refers to Procedural Oriented Programming and its deals with
+programs and functions. Programs are divided into functions and data is 
+global.
+
+OOP v/s POP
+
+1. Defination:
+	OOP => OOP stands for Object Oriented Programing.
+	POP => POP stands for Procedural Oriented Programing.
+
+2. Approach:
+	OOP => It follows bottom up approach.
+	POP => It follows top down approach.
+
+3. Division:
+	OOP => A program is divided into objects and their interactions.
+	POP => A program is divided into functions and they interacts.
+
+4. Inheritance supported:
+	OOP => Inheritance is supported.
+	POP => Inheritance is not supported.
+	
+5. Access Control:
+	OOP => Access control is supported via access modifiers.
+	POP => No access modifiers are supported.
+	
+6. Data Hiding:
+	OOP => Encapsulation is used to hide data.
+	POP => No data hiding is present. Data is globally accesseble.
+	
